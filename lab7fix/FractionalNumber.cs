f7f61870c96630fd456da3b71a0266f32f3b113e
Pavel Takunov <pavel.takynov@gmail.com>
2020-05-23 18:52:17 +0300

    fix

---
+++ lab7fix/FractionalNumber.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Globalization;
+
+namespace Lab7
+{
+    class FractionalNumber : IFormattable, IEquatable<FractionalNumber>, IComparable<FractionalNumber>
+    {
+        public int Denomitor { get; }
+        public int Numerator { get; }
+        public FractionalNumber(int numerator, int denomitor)
+        {
+            int somenumber = Greatest(numerator, denomitor);
+
+            if (denomitor > 0)
+                Denomitor = denomitor / somenumber;
+            else
+                throw new Exception("Знаменатель не может быть равен 0");
+
+            Numerator = numerator / somenumber;
+        }
+        public static FractionalNumber operator +(FractionalNumber group1, FractionalNumber group2)
+            => new FractionalNumber(
+                group1.Numerator * group2.Denomitor + group2.Numerator * group1.Denomitor,
+                group1.Denomitor * group2.Denomitor);
+
+        public static FractionalNumber operator -(FractionalNumber group1, FractionalNumber group2)
+            => new FractionalNumber(
+                group1.Numerator * group2.Denomitor - group2.Numerator * group1.Denomitor,
+                group1.Denomitor * group2.Denomitor);
+
+        public static FractionalNumber operator *(FractionalNumber group1, FractionalNumber group2)
+            => new FractionalNumber(
+                group1.Numerator * group2.Numerator,
+                group1.Denomitor * group2.Denomitor);
+
+        public static FractionalNumber operator /(FractionalNumber group1, FractionalNumber group2)
+            => new FractionalNumber(
+                group1.Numerator * group2.Denomitor,
+                group1.Denomitor * group2.Numerator);
+
+        public static bool operator >(FractionalNumber group1, FractionalNumber group2)
+            => group1.Numerator * group2.Denomitor > group2.Numerator * group1.Denomitor;
+
+        public static bool operator <(FractionalNumber group1, FractionalNumber group2)
+            => group1.Numerator * group2.Denomitor < group2.Numerator * group1.Denomitor;
+
+        public int CompareTo(FractionalNumber group)
+            => (Numerator * group.Denomitor).CompareTo(group.Numerator * Denomitor);
+
+        public bool Equals(FractionalNumber group)
+        {
+            if (group == null)
+                return false;
+            else
+                return Numerator * group.Denomitor == group.Numerator * Denomitor;
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (string.IsNullOrEmpty(format))
+                format = "1";
+            switch (format.ToUpperInvariant())
+            {
+                case "1":
+                    return $"{Numerator}/{Denomitor}";
+                case "2":
+                    return $"{(double)Numerator / Denomitor}";
+                case "3":
+                    return $"{Numerator}\n-\n{Denomitor}";
+                default:
+                    throw new Exception("Неопознанный формат");
+            }
+        }
+
+        private static int Greatest(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            else
+                return Greatest(number2, number1 % number2);
+        }
+
+        public override string ToString()
+        {
+            if (Numerator % Denomitor == 0)
+                return $"{Numerator / Denomitor}";
+            else
+                return ((IFormattable)this).ToString("1", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format) 
+            => ((IFormattable)this).ToString(format, CultureInfo.CurrentCulture);
+
+        public static implicit operator FractionalNumber(int group) => new FractionalNumber(group, 1);
+
+        public static explicit operator int(FractionalNumber group) => group.Numerator / group.Denomitor;
+
+        public static explicit operator double(FractionalNumber group) 
+            => (double)group.Numerator / group.Denomitor;
+    }
+}
---
+++ lab7fix/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        public static int Input()
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int value))
+                    return value;
+                Console.Write("Вы ввели неверное значение, попробуйте заново");
+            }
+        }
+
+        public static void NewFormats(FractionalNumber number1, FractionalNumber number2)
+        {
+            Console.WriteLine($"Ваши числа {number1:1} {number2:1}");
+            Console.WriteLine($"Ваши числа {number1:2} {number2:2}");
+            Console.WriteLine($"Ваши числа\n{number1:3}\n----------\n{number2:3}");
+        }
+
+        public static void Comparison(FractionalNumber number1, FractionalNumber number2)
+        {
+            if (((IEquatable<FractionalNumber>)number1).Equals(number2))
+                Console.WriteLine("Равны");
+            else if (number1 > number2)
+                Console.WriteLine($"{number1} > {number2}");
+            else
+                Console.WriteLine($"{number1} < {number2}");
+        }
+
+        public static void Sort(FractionalNumber group1, FractionalNumber group2)
+        {
+            Console.WriteLine("Отсортированные числа:");
+
+            Array.Sort(new FractionalNumber[] { group1, group2 });
+
+            foreach (var number in new FractionalNumber[] { group1, group2 })
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void NumberInfo(FractionalNumber number)
+        {
+            Console.WriteLine($"Число в виде дроби: {number}");
+            Console.WriteLine($"Число с плавающей точкой: {(double)number}");
+            Console.WriteLine($"Число в целом виде: {(int)number}");
+        }
+
+        public static void DoubleInput(out FractionalNumber group1, out FractionalNumber group2)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите  целый чистлитель и натуральный знаменеметель:");
+
+                    group1 = new FractionalNumber(Input(), Input());
+
+                    Console.WriteLine("Введите  целый чистлитель и натуральный знаменеметель:");
+
+                    group2 = new FractionalNumber(Input(), Input());
+
+                    return;
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.Message);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            FractionalNumber number1 = null;
+            FractionalNumber number2 = null;
+            DoubleInput(out number1, out number2);
+            NewFormats(number1, number2);
+            while (true)
+            {
+                Console.WriteLine("1 +\n2 -\n3 *\n4 /\n5 - выход");
+                switch (Input())
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine($"{number1} + {number2}  = {number1 + number2}");
+                        Comparison(number1, number2);
+                        Sort(number1, number2);
+                        NumberInfo(number1);
+                        NumberInfo(number2);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine($"{number1} - {number2}  = {number1 - number2}");
+                        Comparison(number1, number2);
+                        Sort(number1, number2);
+                        NumberInfo(number1);
+                        NumberInfo(number2);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine($"{number1} * {number2}  = {number1 * number2}");
+                        Comparison(number1, number2);
+                        Sort(number1, number2);
+                        NumberInfo(number1);
+                        NumberInfo(number2);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine($"{number1} / {number2}  = {number1 / number2}");
+                        Comparison(number1, number2);
+                        Sort(number1, number2);
+                        NumberInfo(number1);
+                        NumberInfo(number2);
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+        }
+    }
+}
